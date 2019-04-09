2730e38e2b6c1d6c7877f01a9e2d331e99b59af4
Felix Koch <mail@felixkoch.de>
2019-04-09 20:51:22 +0200

    createStore

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const reducer = (oldState = {}, newState) => {
+  return {...oldState, newState}
+}
+
+const createStore = (reducer) => {
+  let state;
+  
+  const getState = () => state
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+  }
+
+  return {getState, dispatch}
+}
+
+const store = createStore();
+
+const StoreContext =  React.createContext({});
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hallo
+        <StoreContext.Provider value={store}>
+          Hallo
+        </StoreContext.Provider>
       </div>
     );
   }
